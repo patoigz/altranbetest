5e14433fc6a492f8d71b15a358fbbf6376b233be
Patricio Iván García Zitarosa <pgarcia@hubbing.co>
2020-01-30 13:22:34 -0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+const app = express();
+
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function initDB() {
+    const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1/servertestdb';
+    mongoose.set('useCreateIndex', true);
+    mongoose
+        .connect(MONGO_URL, { useNewUrlParser: true })
+        .catch(err => console.error(err))
+        .then(db => {
+            console.log('connected');
+        });
+}
+
+const message = `Server listening at http://${hostname}:${port}`;
+
+app.get('/status', function(req, res) {
+    return res.json({ message: message });
+});
+
+app.listen(port, hostname, function() {
+    console.log(message);
+    initDB();
+});
